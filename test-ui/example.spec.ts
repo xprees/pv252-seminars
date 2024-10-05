952bc205a236c2dd713df7ed3fc5232e69096893
Vaclav Bily <xbily@fi.muni.cz>
2024-10-05 12:03:48 +0200

    example.spec.ts - added sample test

--- test-ui/example.spec.ts
+++ test-ui/example.spec.ts
@@ -10,3 +10,20 @@ test("find-settings", async ({ page }) => {
   await page.goto("/");
   await expect(page.getByTestId("settings")).toBeInViewport();
 });
+
+test("test-my-page-loading-img", async ({ page }) => {
+  await page.goto("https://xprees.com");
+  const img = page.getByAltText("Hero Image");
+  await expect(img).toBeInViewport();
+  await expect(img).toBeVisible();
+});
+
+test("test-my-page-index-title", async ({ page }) => {
+  await page.goto("https://xprees.com/");
+  await expect(page).toHaveTitle(/xprees.*/);
+});
+
+test("test-my-page-projects-title", async ({ page }) => {
+  await page.goto("https://xprees.com/projects");
+  await expect(page).toHaveTitle(/xprees.*/);
+});
