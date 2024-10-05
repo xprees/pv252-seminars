fca85d0d8b23c17cb118adc07cc4139fe2581cd9
Vaclav Bily <xbily@fi.muni.cz>
2024-10-05 12:08:21 +0200

    fibonacci.test.ts added 2 unit tests

--- src/fibonacci.test.ts
+++ src/fibonacci.test.ts
@@ -3,3 +3,11 @@ import { fibonacci } from "./fibonacci.ts";
 test("fibonacci-5", () => {
   expect(fibonacci(5)).toBe(5);
 });
+
+test("fibonacci-10", () => {
+  expect(fibonacci(10)).toBe(55);
+});
+
+test("fibonacci-15", () => {
+  expect(fibonacci(15)).toBe(610);
+});
